6d624a847d14bcd2f7342fbd52b559341abb7b7f
minamaged113 <minamaged113@gmail.com>
2017-11-06 21:58:00 +0200

    mina_Task2_RMS

---
+++ Task_2.m
@@ -0,0 +1,12 @@
+close;
+clc;
+clear;
+load data_high_snr;
+plot(timet,10*log10(abs(data).^2));
+dataPower = 10*log10(abs(data).^2);
+initThreshold = double(rms(data));
+initThreshold = 10*log10(initThreshold^2);
+initMeanVec = initThreshold.*ones(1,max(size(data)));
+hold on
+plot(timet,initMeanVec,'r--');
+disp(['Initial Probability of False Alarm is: ',num2str(qfunc(initThreshold))])
