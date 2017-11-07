fb8989a7a36a4cac964eb4b991601dcdb9f20707
Sercan Turkmen <sercanturkmen@outlook.com>
2017-11-08 00:20:29 +0200

    finished?

--- task2_alt.m
+++ task2_alt.m
@@ -1,4 +1,5 @@
 clearvars; clc; close all; format long;
+
 value_in_dbm = @(val) 10 * log10(abs(val) .^ 2);
 
 load data_high_snr;
@@ -23,7 +24,10 @@ under_threshold_timet = timet(less_than_threshold);
 % but we will use the absolute squared version of the samples so they will
 % act will be distributed exponentially. We tested this by plotting the
 % histogram of the samples
-subplot(2, 1, 1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Signal power, threshold, and H1 samples
+subplot(2, 2, 1);
 
 dbm_threshold_plot = threshold_in_dbm .* ones(size(data));
 plot(timet, data_in_dbm, 'b');
@@ -33,10 +37,14 @@ hold on;
 plot(timet,dbm_threshold_plot,'r--');
 hold on;
 plot(over_threshold_timet, over_threshold_data, 'go');
+
 title("Signal Power");
 legend("data", "threshold dbm", "H1 samples");
 
-subplot(2, 1, 2);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Noise characteristics and pfa
+
+subplot(2, 2, 2);
 
 under_threshold_complex_data = data(abs(data) < abs(threshold));
 noise_abs_sq = abs(under_threshold_complex_data) .^ 2;
@@ -52,3 +60,23 @@ legend("Measured", "Simulated");
 
 pfa = 1 - expcdf(abs(threshold), mean(noise_abs_sq));
 disp("pfa =" + pfa);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Pulse width and period
+subplot(2,2,3);
+
+[pks, locs, w, p] = findpeaks(data_in_dbm, timet, 'MinPeakHeight', threshold_in_dbm, 'MinPeakProminence', 5);
+plot(w, 'o-');
+title("Widths of the pulse");
+hold on;
+
+subplot(2,2,4);
+periods = zeros(1, length(locs)-1);
+for i = 1:(length(locs)-1)
+   periods(1, i) = locs(1, i + 1) - locs(1, i);
+end
+
+plot(periods, 'o-');
+title("Periods of the pulse");
+
+
