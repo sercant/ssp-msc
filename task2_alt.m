fe5297901350f94d7541bccb677a613fc35d2feb
Sercan Turkmen <sercanturkmen@outlook.com>
2017-11-07 23:34:21 +0200

    progress on the task 2

--- task2_alt.m
+++ task2_alt.m
@@ -1,48 +1,54 @@
 clearvars; clc; close all; format long;
+value_in_dbm = @(val) 10 * log10(abs(val) .^ 2);
 
 load data_high_snr;
 
-mf = ones(size(data));
+% energy detector
+threshold = sum(data .^ 2);
 
-value_in_dbm = @(val) 10 * log10(abs(val) .^ 2);
-
-threshold = mf' * data.^2;
 threshold_in_dbm = value_in_dbm(threshold);
-
-dbm_threshold_plot = threshold_in_dbm .* ones(size(data));
-
 data_in_dbm = value_in_dbm(data);
 
+% comparator for seperating samples
 more_than_threshold = data_in_dbm >= threshold_in_dbm;
-
 over_threshold_data = data_in_dbm(more_than_threshold);
 over_threshold_timet = timet(more_than_threshold);
 
+% comparator for seperating samples
 less_than_threshold = data_in_dbm < threshold_in_dbm;
-
 under_threshold_data = data_in_dbm(less_than_threshold);
 under_threshold_timet = timet(less_than_threshold);
 
-under_threshold_complex_data = data(abs(data) < abs(threshold));
-noise_var = var(under_threshold_complex_data);
-disp("noise var=" + noise_var);
+% in-phase quatrature noise samples follow rayleigh distribution
+% but we will use the absolute squared version of the samples so they will
+% act will be distributed exponentially. We tested this by plotting the
+% histogram of the samples
+subplot(2, 1, 1);
 
-% in-phase quatratic samples follow rician distribution
-% but since our noise is 0 mean, it follows rayleigh distribution
-
-% variance of rayleigh dist = (4 - pi) / 2 * sig_sq
-sig_sq = noise_var * 2 / (4 - pi);
-
-% not sure here if i need to use the siq_sq from the equation above
-% or the variance of the noise samples directly.
-pdf = @(x) x / noise_var .* exp(-x .^ 2 / (2 .* noise_var));
-pfa = integral(pdf, abs(threshold), Inf);
-disp("pfa=" + pfa);
-
+dbm_threshold_plot = threshold_in_dbm .* ones(size(data));
 plot(timet, data_in_dbm, 'b');
 hold on;
-plot(over_threshold_timet, over_threshold_data, 'go');
+% plot(under_threshold_timet, under_threshold_data, 'ro');
 % hold on;
-% plot(under_threshold_timet, under_threshold_data, 'r');
-hold on;
 plot(timet,dbm_threshold_plot,'r--');
+hold on;
+plot(over_threshold_timet, over_threshold_data, 'go');
+title("Signal Power");
+legend("data", "threshold dbm", "H1 samples");
+
+subplot(2, 1, 2);
+
+under_threshold_complex_data = data(abs(data) < abs(threshold));
+noise_abs_sq = abs(under_threshold_complex_data) .^ 2;
+
+[p, x] = hist(noise_abs_sq, 16);
+plot(x, p / (sum(p) * (x(2) - x(1))), 'b-');
+hold on;
+
+plot(x, exppdf(x, mean(noise_abs_sq)), 'r-');
+hold on;
+title("Noise Distrubution");
+legend("Measured", "Simulated");
+
+pfa = 1 - expcdf(abs(threshold), mean(noise_abs_sq));
+disp("pfa =" + pfa);
